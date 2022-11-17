26122c69d388c7d87cbfad3df652313a8d56d46a
John Gallo <jgallo@cpg.org>
2022-11-16 20:38:21 -0500

    Completed with local Docker development

---
+++ api/recipe.controllers.js
@@ -0,0 +1,139 @@
+const mongoose = require("mongoose");
+const Recipe = require("./recipe.model");
+// const Recipe = mongoose.model('Recipe');
+
+// Do nothing
+// exports.findAll = function () {};
+
+// Static content
+// exports.findAll = function (req, res) {
+//   res.send([
+//     {
+//       title: "Lasagna",
+//       description:
+//         "Lasagna noodles piled high and layered full of three kinds of cheese to go along with the perfect blend of meaty and zesty, tomato pasta sauce all loaded with herbs.",
+//       image: "lasagna.png",
+//     },
+//   ]);
+// };
+
+// using mongo
+exports.findAll = function (req, res) {
+  Recipe.find({}, function (err, results) {
+    return res.send(results);
+  });
+};
+
+exports.import = function (req, res) {
+  Recipe.create(
+    {
+      title: "Lasagna",
+      description:
+        "Lasagna noodles piled high and layered full of three kinds of cheese to go along with the perfect blend of meaty and zesty, tomato pasta sauce all loaded with herbs.",
+      image: "lasagna.png",
+      ingredients: ["salt", "honey", "sugar", "rice", "walnuts", "lime juice"],
+      preparation: [
+        { step: "Boil water" },
+        { step: "Fry the eggs" },
+        { step: "Serve hot" },
+      ],
+    },
+    {
+      title: "Pho-Chicken Noodle Soup",
+      description:
+        'Pho (pronounced "fuh") is the most popular food in Vietnam, often eaten for breakfast, lunch and dinner. It is made from a special broth that simmers for several hours infused with exotic spices and served over rice noodles with fresh herbs.',
+      image: "pho.png",
+      ingredients: ["salt", "honey", "sugar", "rice", "walnuts", "lime juice"],
+      preparation: [
+        { step: "Boil water" },
+        { step: "Fry the eggs" },
+        { step: "Serve hot" },
+      ],
+    },
+    {
+      title: "Guacamole",
+      description:
+        "Guacamole is definitely a staple of Mexican cuisine. Even though Guacamole is pretty simple, it can be tough to get the perfect flavor - with this authentic Mexican guacamole recipe, though, you will be an expert in no time.",
+      image: "guacamole.png",
+      ingredients: ["salt", "honey", "sugar", "rice", "walnuts", "lime juice"],
+      preparation: [
+        { step: "Boil water" },
+        { step: "Fry the eggs" },
+        { step: "Serve hot" },
+      ],
+    },
+    {
+      title: "Hamburger",
+      description:
+        "A Hamburger (often called a burger) is a type of sandwich in the form of  rounded bread sliced in half with its center filled with a patty which is usually ground beef, then topped with vegetables such as lettuce, tomatoes and onions.",
+      image: "hamburger.png",
+      ingredients: ["salt", "honey", "sugar", "rice", "walnuts", "lime juice"],
+      preparation: [
+        { step: "Boil water" },
+        { step: "Fry the eggs" },
+        { step: "Serve hot" },
+      ],
+    },
+    function (err) {
+      if (err) return console.log(err);
+      return res.sendStatus(202);
+    }
+  );
+};
+
+exports.killall = function (req, res) {
+  //   Recipe.deleteMany({ title: "Lasagna" }, (err) => {
+  Recipe.deleteMany({}, (err) => {
+    if (err) return console.log(err);
+    return res.sendStatus(202);
+  });
+};
+
+// exports.findById = function () {};
+exports.findById = (req, res) => {
+  const id = req.params.id;
+  Recipe.findOne({ _id: id }, (err, json) => {
+    if (err) return console.log(err);
+    return res.send(json);
+  });
+};
+
+// exports.add = function () {};
+exports.add = function (req, res) {
+  Recipe.create(req.body, function (err, recipe) {
+    if (err) return console.log(err);
+    return res.send(recipe);
+  });
+};
+
+// exports.delete = function () {};
+exports.delete = function (req, res) {
+  let id = req.params.id;
+  Recipe.deleteOne({ _id: id }, () => {
+    return res.sendStatus(202);
+  });
+};
+
+// exports.update = function () {};
+exports.update = function (req, res) {
+  console.log(req.body);
+  const id = req.params.id;
+  Recipe.findByIdAndUpdate(id, req.body, { new: true }, (err, response) => {
+    if (err) return console.log(err);
+    res.send(response);
+  });
+};
+exports.upload = function (req, res) {
+  console.log(req.files);
+  if (Object.keys(req.files).length == 0) {
+    return res.status(400).send("No files were uploaded.");
+  }
+  let file = req.files.file;
+  file.mv(`./public/img/${req.body.filename}`, (err) => {
+    if (err) {
+      return res.status(500).send(err);
+    }
+    res.json({ file: `public/img/${req.body.filename}` });
+    console.log(res.json);
+  });
+};
--- server.js
+++ server.js
@@ -1,5 +1,8 @@
 const express = require("express");
 const mongoose = require("mongoose");
+const recipeControllers = require("./api/recipe.controllers");
+
+const fileUpload = require("express-fileupload");
 
 const app = express();
 
@@ -32,59 +35,78 @@ mongoose
   .then(() => console.log("MongoDb connected"))
   .catch((err) => console.log(err));
 
-const RecipeSchema = new mongoose.Schema({
-  title: String,
-  description: String,
-  image: String,
-});
+// in recipe.model
+// const RecipeSchema = new mongoose.Schema({
+//   title: String,
+//   description: String,
+//   image: String,
+// });
 
-const Recipe = mongoose.model("Recipe", RecipeSchema);
+// const Recipe = mongoose.model("Recipe", RecipeSchema);
 
 // Index.html entry
-app.get("/", function (req, res) {
-  res.sendFile(__dirname + "/static/index.html");
-});
+// app.get("/", function (req, res) {
+//   res.sendFile(__dirname + "/static/index.html");
+// });
+
+// Middleware (app.use)
+// Specify where the 'static' folder is
+app.use(express.static("public"));
+app.use(express.json({ extended: false }));
+app.use(express.urlencoded({ extended: false }));
+app.use(fileUpload());
 
 // Display Recipes
-app.get("/api/recipes", function (req, res) {
-  Recipe.find({}, function (err, results) {
-    return res.send(results);
-  });
-});
+// Using the one below
+// app.get("/api/recipes", function (req, res) {
+//   Recipe.find({}, function (err, results) {
+//     return res.send(results);
+//   });
+// });
+
+app.get("/api/recipes", recipeControllers.findAll);
+app.get("/api/recipes/:id", recipeControllers.findById);
+app.post("/api/recipes", recipeControllers.add);
+app.put("/api/recipes/:id", recipeControllers.update);
+app.delete("/api/recipes/:id", recipeControllers.delete);
+app.get("/api/import", recipeControllers.import);
+app.get("/api/killall", recipeControllers.killall);
+
+app.post("/api/upload", recipeControllers.upload);
 
 // Pass some data to the database using model.create(), a shortcut for saving one or more documents to the database:
-app.get("/api/import", (req, res) => {
-  Recipe.create(
-    {
-      title: "Lasagna",
-      description:
-        "Lasagna noodles piled high and layered full of three kinds of cheese to go along with the perfect blend of meaty and zesty, tomato pasta sauce all loaded with herbs.",
-      image: "lasagna.png",
-    },
-    {
-      title: "Pho-Chicken Noodle Soup",
-      description:
-        'Pho (pronounced "fuh") is the most popular food in Vietnam, often eaten for breakfast, lunch and dinner. It is made from a special broth that simmers for several hours infused with exotic spices and served over rice noodles with fresh herbs.',
-      image: "pho.png",
-    },
-    {
-      title: "Guacamole",
-      description:
-        "Guacamole is definitely a staple of Mexican cuisine. Even though Guacamole is pretty simple, it can be tough to get the perfect flavor - with this authentic Mexican guacamole recipe, though, you will be an expert in no time.",
-      image: "guacamole.png",
-    },
-    {
-      title: "Hamburger",
-      description:
-        "A Hamburger (often called a burger) is a type of sandwich in the form of  rounded bread sliced in half with its center filled with a patty which is usually ground beef, then topped with vegetables such as lettuce, tomatoes and onions.",
-      image: "hamburger.png",
-    },
-    function (err) {
-      if (err) return console.log(err);
-      return res.sendStatus(201);
-    }
-  );
-});
+// app.get("/api/import", (req, res) => {
+//   Recipe.create(
+//     {
+//       title: "Lasagna",
+//       description:
+//         "Lasagna noodles piled high and layered full of three kinds of cheese to go along with the perfect blend of meaty and zesty, tomato pasta sauce all loaded with herbs.",
+//       image: "lasagna.png",
+//     },
+//     {
+//       title: "Pho-Chicken Noodle Soup",
+//       description:
+//         'Pho (pronounced "fuh") is the most popular food in Vietnam, often eaten for breakfast, lunch and dinner. It is made from a special broth that simmers for several hours infused with exotic spices and served over rice noodles with fresh herbs.',
+//       image: "pho.png",
+//     },
+//     {
+//       title: "Guacamole",
+//       description:
+//         "Guacamole is definitely a staple of Mexican cuisine. Even though Guacamole is pretty simple, it can be tough to get the perfect flavor - with this authentic Mexican guacamole recipe, though, you will be an expert in no time.",
+//       image: "guacamole.png",
+//     },
+//     {
+//       title: "Hamburger",
+//       description:
+//         "A Hamburger (often called a burger) is a type of sandwich in the form of  rounded bread sliced in half with its center filled with a patty which is usually ground beef, then topped with vegetables such as lettuce, tomatoes and onions.",
+//       image: "hamburger.png",
+//     },
+//     function (err) {
+//       if (err) return console.log(err);
+//       return res.sendStatus(201);
+//     }
+//   );
+// });
 
 // const PORT = 3000;
 const PORT = process.env.PORT || 3000;
